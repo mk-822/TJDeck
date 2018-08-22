5542265f8e193fc4de6bc45ee1b370d195a9ed75
pumpkins <7.methyltheophylline@gmail.com>
2018-08-23 02:42:16 +0900

    keep screen on

--- app/src/main/java/net/totoraj/tjdeck/MainActivity.java
+++ app/src/main/java/net/totoraj/tjdeck/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
@@ -50,7 +51,7 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
 
-
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         /* jsの用意 */
         try {
