37696e224d6d90438fc6234de772911c2c95d629
pumpkins <7.methyltheophylline@gmail.com>
2018-08-23 15:41:20 +0900

    refactor

--- app/src/main/java/net/totoraj/tjdeck/MainActivity.kt
+++ app/src/main/java/net/totoraj/tjdeck/MainActivity.kt
@@ -1,49 +1,47 @@
 package net.totoraj.tjdeck
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
-import android.content.MutableContextWrapper
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.os.Handler
 import android.os.Looper
 import android.provider.MediaStore
+import android.text.format.DateFormat
 import android.util.Log
 import android.view.KeyEvent
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.view.Window
 import android.view.WindowManager
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebResourceRequest
-import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
 import java.io.BufferedReader
 import java.io.File
 import java.io.IOException
-import java.io.InputStream
 import java.io.InputStreamReader
-import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.regex.Pattern
 
 class MainActivity : Activity() {
-    private val TAG = "TJDeck"
-    private val mContent: View? = null
+
+    companion object {
+        const val TAG = "TJDeck"
+        const val INPUT_FILE_REQUEST_CODE = 1
+        // const val EXTRA_FROM_NOTIFICATION = "EXTRA_FORM_NOTIFICATION"
+
+        const val TWEET_DECK = "https://tweetdeck.twitter.com"
+    }
+
     private var mWebView: WebView? = null
-    private var tjDeckScript = ""
-    private var tjCheckScript = ""
-    private val regexDeck = "^https://tweetdeck\\.twitter\\.com.*$"
-    private val regexTwitter = "^https://(.+\\.|)twitter\\.com/(login|logout|sessions|account/login_verification).*$"
     private var mFilePathCallback: ValueCallback<Array<Uri>>? = null
     private var mCameraPhotoPath: String? = null
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -53,20 +51,6 @@ class MainActivity : Activity() {
             if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
                 hideSystemUI(1000)
             }
-        }
-
-        /* jsの用意 */
-        try {
-            if (BuildConfig.DEBUG) {
-                Log.d(TAG, "デバッグ用")
-                tjDeckScript = loadAssets("tj-deck-debug.js")
-            } else {
-                Log.d(TAG, "リリース用")
-                tjDeckScript = loadAssets("tj-deck.js")
-            }
-            tjCheckScript = loadAssets("test.js")
-        } catch (error: Exception) {
-            Log.d(TAG, "onError")
         }
 
         mWebView = findViewById<View>(R.id.webView1) as WebView
@@ -84,28 +68,41 @@ class MainActivity : Activity() {
         settings.displayZoomControls = false
 
         if (savedInstanceState == null) {
-            mWebView!!.loadUrl("https://tweetdeck.twitter.com")
+            mWebView!!.loadUrl(TWEET_DECK)
         }
+    }
 
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            hideSystemUI(0)
+        }
+    }
 
+    private fun hideSystemUI(delayMillis: Int) {
+        val decorView = window.decorView
+        Handler(Looper.getMainLooper()).postDelayed({
+            decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        }, delayMillis.toLong())
     }
 
     /* アセットからjsファイルを読み込んでStringで返すやつ */
     @Throws(Exception::class)
     private fun loadAssets(fileName: String): String {
         val res = StringBuilder()
-        var `is`: InputStream? = null
-        var br: BufferedReader? = null
-        `is` = assets.open(fileName)
-        br = BufferedReader(InputStreamReader(`is`!!, "UTF-8"))
+        val br = BufferedReader(InputStreamReader(assets.open(fileName), "UTF-8"))
 
-        for(line in br.readLines()){
+        for (line in br.readLines()) {
             res.append(line)
         }
 
         return res.substring(0)
     }
-
 
     /* 戻るボタンでブラウザバックするようにする */
     override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
@@ -122,13 +119,13 @@ class MainActivity : Activity() {
         mWebView!!.saveState(outState)
     }
 
-    /* WebViewの内容を保持する */
+    /* WebViewの保持した内容を戻す */
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         mWebView!!.restoreState(savedInstanceState)
     }
 
-    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode != INPUT_FILE_REQUEST_CODE || mFilePathCallback == null) {
             super.onActivityResult(requestCode, resultCode, data)
             return
@@ -156,7 +153,6 @@ class MainActivity : Activity() {
         return
     }
 
-
     // https://github.com/googlearchive/chromium-webview-samples/blob/master/input-file-example/app/src/main/java/inputfilesample/android/chrome/google/com/inputfilesample/MainFragment.java
     inner class TJChromeClient : WebChromeClient() {
         override fun onShowFileChooser(
@@ -167,21 +163,21 @@ class MainActivity : Activity() {
             }
             mFilePathCallback = filePathCallback
 
-            var takePicutureIntent: Intent? = Intent(MediaStore.INTENT_ACTION_MEDIA_SEARCH)
-            if (takePicutureIntent!!.resolveActivity(packageManager) != null) {
+            var takePictureIntent: Intent? = Intent(MediaStore.INTENT_ACTION_MEDIA_SEARCH)
+            if (takePictureIntent!!.resolveActivity(packageManager) != null) {
                 var photoFile: File? = null
                 try {
                     photoFile = createImageFile()
-                    takePicutureIntent.putExtra("PhotoPath", mCameraPhotoPath)
+                    takePictureIntent.putExtra("PhotoPath", mCameraPhotoPath)
                 } catch (error: IOException) {
 
                 }
 
                 if (photoFile != null) {
                     mCameraPhotoPath = "file:" + photoFile.absolutePath
-                    takePicutureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile))
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile))
                 } else {
-                    takePicutureIntent = null
+                    takePictureIntent = null
                 }
             }
 
@@ -189,11 +185,10 @@ class MainActivity : Activity() {
             contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE)
             contentSelectionIntent.type = "*/*"
 
-            val intentArray: Array<Intent>
-            if (takePicutureIntent != null) {
-                intentArray = arrayOf(takePicutureIntent)
+            val intentArray: Array<Intent> = if (takePictureIntent != null) {
+                arrayOf(takePictureIntent)
             } else {
-                intentArray = arrayOf()
+                arrayOf()
             }
 
             val chooserIntent = Intent(Intent.ACTION_CHOOSER)
@@ -201,8 +196,8 @@ class MainActivity : Activity() {
             chooserIntent.putExtra(Intent.EXTRA_TITLE, "Media Chooser")
             chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray)
             chooserIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
-            val mimetypes = arrayOf("image/*", "video/*")
-            chooserIntent.putExtra(Intent.EXTRA_MIME_TYPES, mimetypes)
+            val mimeTypes = arrayOf("image/*", "video/*")
+            chooserIntent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
 
             startActivityForResult(chooserIntent, INPUT_FILE_REQUEST_CODE)
 
@@ -211,7 +206,7 @@ class MainActivity : Activity() {
 
         @Throws(IOException::class)
         private fun createImageFile(): File {
-            val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+            val timeStamp = DateFormat.format("yyyyMMdd_HHmmss", Date()).toString()
             val storageDir = Environment.getExternalStoragePublicDirectory(
                     Environment.DIRECTORY_DOCUMENTS)
             return File.createTempFile(timeStamp, "*", storageDir)
@@ -221,6 +216,27 @@ class MainActivity : Activity() {
 
     /* WebViewのクライアント */
     inner class TJClient : WebViewClient() {
+        private val regexDeck = "^https://tweetdeck\\.twitter\\.com.*$"
+        private val regexTwitter = "^https://(.+\\.|)twitter\\.com/(login|logout|sessions|account/login_verification).*$"
+        private var tjDeckScript = ""
+        private var tjCheckScript = ""
+
+        init {
+            /* jsの用意 */
+            try {
+                tjDeckScript = if (BuildConfig.DEBUG) {
+                    Log.d(TAG, "デバッグ用")
+                    loadAssets("tj-deck-debug.js")
+                } else {
+                    Log.d(TAG, "リリース用")
+                    loadAssets("tj-deck.js")
+                }
+                tjCheckScript = loadAssets("test.js")
+            } catch (error: Exception) {
+                Log.d(TAG, "onError")
+            }
+        }
+
         // ページのロード終了時
         override fun onPageFinished(view: WebView, url: String) {
             super.onPageFinished(view, url)
@@ -261,51 +277,26 @@ class MainActivity : Activity() {
             val itDeck = Pattern.matches(regexDeck, url)
             val itTwiLogin = Pattern.matches(regexTwitter, url)
             Log.d(TAG, url)
-            if (itDeck) {
-                Log.d(TAG, "TweetDeckです")
+            when {
+                itDeck -> {
+                    Log.d(TAG, "TweetDeckです")
 
-                // TweetDeckにログインしていてなおかつtj-deckが実行されていないか確認する
-                view.evaluateJavascript(tjCheckScript) { value ->
-                    Log.d(TAG, value)
-                    if (java.lang.Boolean.parseBoolean(value)) {
-                        // 実行！！
-                        view.evaluateJavascript(tjDeckScript, null)
-                    } else if (Pattern.matches("^.*/\\?via_twitter_login=true$", url)) {
-                        Log.d(TAG, "via_twitter_login=true")
-                        view.evaluateJavascript(tjDeckScript, null)
+                    // TweetDeckにログインしていてなおかつtj-deckが実行されていないか確認する
+                    view.evaluateJavascript(tjCheckScript) { value ->
+                        Log.d(TAG, value)
+                        if (java.lang.Boolean.parseBoolean(value)) {
+                            // 実行！！
+                            view.evaluateJavascript(tjDeckScript, null)
+                        } else if (Pattern.matches("^.*/\\?via_twitter_login=true$", url)) {
+                            Log.d(TAG, "via_twitter_login=true")
+                            view.evaluateJavascript(tjDeckScript, null)
+                        }
                     }
                 }
-            } else if (itTwiLogin) {
-                Log.d(TAG, "Twitterのログインページです")
-            } else {
-                Log.d(TAG, "それ以外です")
+                itTwiLogin -> Log.d(TAG, "Twitterのログインページです")
+                else -> Log.d(TAG, "それ以外です")
             }
         }
 
-    }
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) {
-            hideSystemUI(0)
-        }
-    }
-
-    private fun hideSystemUI(delayMillis: Int) {
-        val decorView = window.decorView
-        Handler(Looper.getMainLooper()).postDelayed({
-            decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
-                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
-        }, delayMillis.toLong())
-    }
-
-    companion object {
-
-        val INPUT_FILE_REQUEST_CODE = 1
-        val EXTRA_FROM_NOTIFICATION = "EXTRA_FORM_NOTIFICATION"
     }
 }
