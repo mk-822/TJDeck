325b7241c45b5a10f6c6daf2081802d1a4728d61
pumpkins <7.methyltheophylline@gmail.com>
2019-03-25 11:17:31 +0900

    リファクタリング

--- app/src/main/java/net/totoraj/tjdeck/adapter/UploadItemAdapter.kt
+++ app/src/main/java/net/totoraj/tjdeck/adapter/UploadItemAdapter.kt
@@ -2,19 +2,16 @@ package net.totoraj.tjdeck.adapter
 
 import android.net.Uri
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.layout_upload_item.view.*
 import net.totoraj.tjdeck.MyApplication.Companion.getAppContext
 import net.totoraj.tjdeck.R
 import net.totoraj.tjdeck.view.UploadItemDiffCallback
-import net.totoraj.tjdeck.view.UploadItemViewHolder
 
 class UploadItemAdapter(private var items: List<Uri>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-    interface UploadItemAdapterListener {
-        fun deleteItem(item: Uri)
-    }
 
     fun updateItems(newItems: List<Uri>) {
         val diffResult = DiffUtil.calculateDiff(UploadItemDiffCallback(items, newItems))
@@ -36,4 +33,8 @@ class UploadItemAdapter(private var items: List<Uri>) : RecyclerView.Adapter<Rec
                 else -> { // do nothing
                 }
             }
+
+    inner class UploadItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(uri: Uri) = view.img_preview.setImageURI(uri)
+    }
 }
